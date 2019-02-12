3a47442a94bc9e100969cebaca9c5ba230d3ca83
Hanxing Zhang <35745247+681starflower@users.noreply.github.com>
2019-02-12 12:02:41 -0800

    updated 8 queries

--- src/views/feed_screen.js
+++ src/views/feed_screen.js
@@ -40,12 +40,12 @@ class FeedScreen extends Component {
     console.log("hello");
     const db = firebase.firestore();
 
-    //query 1: given a key for users, get the users detail
-    var docRef = db.collection("users").doc("MLVHiDjnWdVrw6QAr3fU");
-
-    docRef.get().then(function(doc) {
+    //query 1: user profile details for user MLVHiDjnWdVrw6QAr3fU is
+    db.collection("users")
+        .doc("MLVHiDjnWdVrw6QAr3fU")
+        .get().then(function(doc) {
       if (doc.exists) {
-        console.log("user data is :", doc.data());
+        console.log("user profile data for MLVHiDjnWdVrw6QAr3fU is :", doc.data());
       } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
@@ -54,19 +54,17 @@ class FeedScreen extends Component {
       console.log("Error getting document:", error);
     });
 
-    console.log("after_hello_feed_screen_q1");
+
 
     //query 2: given a user, find all posts the user made
-    // Create a reference to the posts collection
-    var postsRef = db.collection("posts");
 
-    // Create a query against the collection.
-    db.collection("posts").where("post_userID", "==", "MLVHiDjnWdVrw6QAr3fU")
+    db.collection("posts")
+        .where("post_userID", "==", "MLVHiDjnWdVrw6QAr3fU")
         .get()
         .then(function(querySnapshot) {
           querySnapshot.forEach(function(doc) {
             // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
+            console.log("MLVHiDjnWdVrw6QAr3fU posted these pictures: ", doc.data().image_url);
           });
         })
         .catch(function(error) {
@@ -74,14 +72,14 @@ class FeedScreen extends Component {
         });
 
 
-    console.log("after_hello_feed_screen_q2");
 
     //query 3: given a user, find all users the user follows
-    db.collection("followed").doc("t3umcEmI187GfkibsYj7")
+    db.collection("followed")
+        .doc("t3umcEmI187GfkibsYj7")
         .get()
         .then(function(doc) {
           if (doc.exists) {
-            console.log("the users the app user follows are:", doc.data());
+            console.log("app user t3umcEmI187GfkibsYj7 follows these users:", doc.data());
           } else {
             // doc.data() will be undefined in this case
             console.log("No such document!");
@@ -91,10 +89,86 @@ class FeedScreen extends Component {
     });
 
 
-    //query 4: given a user, find all posts from the people that the user follows
+    //query 4: given a user t3umcEmI187GfkibsYj7, find all posts from the people that the user follows
+    db.collection("posts")
+        .where("followers_ID", "array-contains", "t3umcEmI187GfkibsYj7")
+        .get()
+        .then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log("all posts should be shown for t3umcEmI187GfkibsYj7 are ", doc.data().image_url);
+          });
+        })
+        .catch(function(error) {
+          console.log("Error getting documents: ", error);
+        });
 
-    console.log("after_hello_feed_screen_q3");
 
+
+
+    // query 5: given a hashtag, find all posts with this hashtag
+
+    db.collection("posts")
+        .where("hashtag", "array-contains", "snow")
+        .get()
+        .then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log("posts contains hashtag \"snow\" are => ",  doc.data().image_url, doc.data().hashtag);
+          });
+        })
+        .catch(function(error) {
+          console.log("Error getting documents: ", error);
+        });
+
+
+    // query 6: given a post, find all comments for this post
+
+    db.collection("posts")
+        .doc("KOTXpXKf1BagqattgKt5")
+        .get()
+        .then(function(doc) {
+
+            // doc.data() is never undefined for query doc snapshots
+            console.log("post KOTXpXKf1BagqattgKt5 has these comments: ", doc.data().commented_by_user);
+
+        })
+        .catch(function(error) {
+          console.log("Error getting documents: ", error);
+        });
+
+    // query 7: given a post, find the followers that liked this post
+
+    db.collection("posts")
+        .doc("KOTXpXKf1BagqattgKt5")
+        .get()
+        .then(function(doc) {
+
+          // doc.data() is never undefined for query doc snapshots
+          // console.log("these followers liked post KOTXpXKf1BagqattgKt5: ", doc.id, doc.data().liked_by_users);
+          console.log("these followers liked post KOTXpXKf1BagqattgKt5: ", doc.data().liked_by_users);
+
+        })
+        .catch(function(error) {
+          console.log("Error getting documents: ", error);
+        });
+
+
+    // query 8: given a user, find all the posts that the user liked
+
+    db.collection("posts")
+        .where("liked_by_users", "array-contains", "t3umcEmI187GfkibsYj7")
+        .get()
+        .then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            // console.log("t3umcEmI187GfkibsYj7 liked these posts => ",doc.id,  doc.data().image_url);
+            console.log("t3umcEmI187GfkibsYj7 liked these posts => ",  doc.data().image_url);
+          });
+        })
+        .catch(function(error) {
+          console.log("Error getting documents: ", error);
+        });
 
 
     return (
