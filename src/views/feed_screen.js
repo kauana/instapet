bedf2e48ac2ae307ff5baf25e1f3dbd970bd1710
Hanxing Zhang <35745247+681starflower@users.noreply.github.com>
2019-02-20 22:29:57 -0800

    added function to retrieve most recent K posts from each friend the user follows

--- src/views/feed_screen.js
+++ src/views/feed_screen.js
@@ -4,153 +4,222 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import firebase from '../../firestore';
 
 class FeedScreen extends Component {
-  static navigationOptions = () => ({
-    tabBarIcon: ({ tintColor }) => (
-      <Icon
-        name="home"
-        size={24}
-        color={tintColor}
-      />
-    ),
-  })
+    static navigationOptions = () => ({
+        tabBarIcon: ({ tintColor }) => (
+            <Icon
+                name="home"
+                size={24}
+                color={tintColor}
+            />
+        ),
+    })
 
-  render() {
-    // query against cloud store starts here
-    console.log('hello');
-    const db = firebase.firestore();
+    render() {
+        // query against cloud store starts here
+        const db = firebase.firestore();
 
-    // query 1: user profile details for user MLVHiDjnWdVrw6QAr3fU is
-    db.collection('users')
-      .doc('MLVHiDjnWdVrw6QAr3fU')
-      .get().then((doc) => {
-        if (doc.exists) {
-          console.log('user profile data for MLVHiDjnWdVrw6QAr3fU is :', doc.data());
-        } else {
-          // doc.data() will be undefined in this case
-          console.log('No such document!');
+        // sortedObjected are posts sorted by time stamps
+        // show_TopK returns most recent K posts from each person app_user_id follows
+        function show_TopK(sortedObject,app_user_id,topK) {
+
+            var filteredObject = [];
+
+            // filteredObject are posts from people app_user_id follows
+            sortedObject.forEach((doc) => {
+                if (doc.data().followers_ID.indexOf(app_user_id) > -1) {
+                    filteredObject.push(doc);
+                }
+            });
+
+            console.log("filterd for app user");
+
+            var postFreqDict = {}
+            var topKfeeds = []
+            filteredObject.forEach((doc) => {
+                if (doc.data().post_userID in postFreqDict) {
+                    postFreqDict[doc.data().post_userID] += 1;
+                } else {
+                    postFreqDict[doc.data().post_userID] = 1;
+                }
+
+                if (postFreqDict[doc.data().post_userID] <= topK) {
+                    topKfeeds.push(doc)
+                }
+            });
+            console.log("after top K");
+
+            topKfeeds.forEach((doc) => {
+                console.log('most recent', topK,  'posts from each person t3umcEmI187GfkibsYj7 follows are ',
+                    doc.data().image_url, doc.data().post_userID, doc.data().post_time_stamp);
+            });
+
         }
-      })
-      .catch((error) => {
-        console.log('Error getting document:', error);
-      });
+
+        /*  // query 1: user profile details for user MLVHiDjnWdVrw6QAr3fU is
+         db.collection('users')
+           .doc('MLVHiDjnWdVrw6QAr3fU')
+           .get().then((doc) => {
+             if (doc.exists) {
+               console.log('user profile data for MLVHiDjnWdVrw6QAr3fU is :', doc.data());
+             } else {
+               // doc.data() will be undefined in this case
+               console.log('No such document!');
+             }
+           })
+           .catch((error) => {
+             console.log('Error getting document:', error);
+           });
 
 
-    // query 2: given a user, find all posts the user made
-    db.collection('posts')
-      .where('post_userID', '==', 'MLVHiDjnWdVrw6QAr3fU')
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log('MLVHiDjnWdVrw6QAr3fU posted these pictures: ', doc.data().image_url);
-        });
-      })
-      .catch((error) => {
-        console.log('Error getting documents: ', error);
-      });
+         // query 2: given a user, find all posts the user made
+         db.collection('posts')
+           .where('post_userID', '==', 'MLVHiDjnWdVrw6QAr3fU')
+           .get()
+           .then((querySnapshot) => {
+             querySnapshot.forEach((doc) => {
+               // doc.data() is never undefined for query doc snapshots
+               console.log('MLVHiDjnWdVrw6QAr3fU posted these pictures: ', doc.data().image_url);
+             });
+           })
+           .catch((error) => {
+             console.log('Error getting documents: ', error);
+           });
 
 
-    // query 3: given a user, find all users the user follows
-    db.collection('followed')
-      .doc('t3umcEmI187GfkibsYj7')
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          console.log('app user t3umcEmI187GfkibsYj7 follows these users:', doc.data());
-        } else {
-          // doc.data() will be undefined in this case
-          console.log('No such document!');
-        }
-      })
-      .catch((error) => {
-        console.log('Error getting document:', error);
-      });
+         // query 3: given a user, find all users the user follows
+         db.collection('followed')
+           .doc('t3umcEmI187GfkibsYj7')
+           .get()
+           .then((doc) => {
+             if (doc.exists) {
+               console.log('app user t3umcEmI187GfkibsYj7 follows these users:', doc.data());
+             } else {
+               // doc.data() will be undefined in this case
+               console.log('No such document!');
+             }
+           })
+           .catch((error) => {
+             console.log('Error getting document:', error);
+           });
 
 
-    // query 4: given a user t3umcEmI187GfkibsYj7, find all posts from the people that the user follows
-    db.collection('posts')
-      .where('followers_ID', 'array-contains', 't3umcEmI187GfkibsYj7')
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log('all posts should be shown for t3umcEmI187GfkibsYj7 are ', doc.data().image_url);
-        });
-      })
-      .catch((error) => {
-        console.log('Error getting documents: ', error);
-      });
+         // query 4: given a user t3umcEmI187GfkibsYj7, find all posts from the people that the user follows
+         db.collection('posts')
+           .where('followers_ID', 'array-contains', 't3umcEmI187GfkibsYj7')
+           .get()
+           .then((querySnapshot) => {
+             querySnapshot.forEach((doc) => {
+               // doc.data() is never undefined for query doc snapshots
+               console.log('all posts should be shown for t3umcEmI187GfkibsYj7 are ', doc.data().image_url, doc.data().post_time_stamp);
+             });
+           })
+           .catch((error) => {
+             console.log('Error getting documents: ', error);
+           });
 
 
-    // query 5: given a hashtag, find all posts with this hashtag
-    db.collection('posts')
-      .where('hashtag', 'array-contains', 'snow')
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log('posts contains hashtag "snow" are => ', doc.data().image_url, doc.data().hashtag);
-        });
-      })
-      .catch((error) => {
-        console.log('Error getting documents: ', error);
-      });
+         // query 5: given a hashtag, find all posts with this hashtag
+         db.collection('posts')
+           .where('hashtag', 'array-contains', 'snow')
+           .get()
+           .then((querySnapshot) => {
+             querySnapshot.forEach((doc) => {
+               // doc.data() is never undefined for query doc snapshots
+               console.log('posts contains hashtag "snow" are => ', doc.data().image_url, doc.data().hashtag);
+             });
+           })
+           .catch((error) => {
+             console.log('Error getting documents: ', error);
+           });
 
 
-    // query 6: given a post, find all comments for this post
-    db.collection('posts')
-      .doc('KOTXpXKf1BagqattgKt5')
-      .get()
-      .then((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log('post KOTXpXKf1BagqattgKt5 has these comments: ', doc.data().commented_by_user);
-      })
-      .catch((error) => {
-        console.log('Error getting documents: ', error);
-      });
+         // query 6: given a post, find all comments for this post
+         db.collection('posts')
+           .doc('KOTXpXKf1BagqattgKt5')
+           .get()
+           .then((doc) => {
+             // doc.data() is never undefined for query doc snapshots
+             console.log('post KOTXpXKf1BagqattgKt5 has these comments: ', doc.data().commented_by_user);
+           })
+           .catch((error) => {
+             console.log('Error getting documents: ', error);
+           });
 
-    // query 7: given a post, find the followers that liked this post
-    db.collection('posts')
-      .doc('KOTXpXKf1BagqattgKt5')
-      .get()
-      .then((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        // console.log("these followers liked post KOTXpXKf1BagqattgKt5: ", doc.id, doc.data().liked_by_users);
-        console.log('these followers liked post KOTXpXKf1BagqattgKt5: ', doc.data().liked_by_users);
-      })
-      .catch((error) => {
-        console.log('Error getting documents: ', error);
-      });
-
-
-    // query 8: given a user, find all the posts that the user liked
-    db.collection('posts')
-      .where('liked_by_users', 'array-contains', 't3umcEmI187GfkibsYj7')
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          // console.log("t3umcEmI187GfkibsYj7 liked these posts => ",doc.id,  doc.data().image_url);
-          console.log('t3umcEmI187GfkibsYj7 liked these posts => ', doc.data().image_url);
-        });
-      })
-      .catch((error) => {
-        console.log('Error getting documents: ', error);
-      });
+         // query 7: given a post, find the followers that liked this post
+         db.collection('posts')
+           .doc('KOTXpXKf1BagqattgKt5')
+           .get()
+           .then((doc) => {
+             // doc.data() is never undefined for query doc snapshots
+             // console.log("these followers liked post KOTXpXKf1BagqattgKt5: ", doc.id, doc.data().liked_by_users);
+             console.log('these followers liked post KOTXpXKf1BagqattgKt5: ', doc.data().liked_by_users);
+           })
+           .catch((error) => {
+             console.log('Error getting documents: ', error);
+           });
 
 
-    return (
-      <View style={{
-        flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center',
-      }}
-      >
-        <Text>Hello!</Text>
-        <Text>This is the main feed.</Text>
-        <Icon name="wrench" size={32} />
-        <Text>Under construction.</Text>
-      </View>
-    );
-  }
+         // query 8: given a user, find all the posts that the user liked
+         db.collection('posts')
+           .where('liked_by_users', 'array-contains', 't3umcEmI187GfkibsYj7')
+           .get()
+           .then((querySnapshot) => {
+             querySnapshot.forEach((doc) => {
+               // doc.data() is never undefined for query doc snapshots
+               // console.log("t3umcEmI187GfkibsYj7 liked these posts => ",doc.id,  doc.data().image_url);
+               console.log('t3umcEmI187GfkibsYj7 liked these posts => ', doc.data().image_url);
+             });
+           })
+           .catch((error) => {
+             console.log('Error getting documents: ', error);
+           });
+      */
+
+
+
+        // test order by post_time_stamp
+        // query 9: show all posts, ordered by post_time_stamp
+        db.collection('posts')
+            .orderBy("post_time_stamp", 'desc')
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log('all posts order by post_time_stamp are ', doc.data().image_url, doc.data().post_time_stamp);
+                });
+            })
+            .catch((error) => {
+                console.log('Error getting documents: ', error);
+            });
+
+
+
+        // For app user t3umcEmI187GfkibsYj7, show most recent topK posts from
+        // each person t3umcEmI187GfkibsYj7 follows
+        const temp_order_by_time = db.collection('posts')
+            .orderBy('post_time_stamp', "desc");
+
+        var topK = 2;
+        temp_order_by_time.get()
+            .then((querySnapshot) => {
+                show_TopK(querySnapshot, "t3umcEmI187GfkibsYj7", topK)
+            })
+            .catch((error) => {
+                console.log('Error getting documents: ', error);
+            });
+
+        return (
+            <View style={{
+                flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center',
+            }}
+            >
+                <Text>Hello!</Text>
+                <Text>This is the main feed.</Text>
+                <Icon name="wrench" size={32} />
+                <Text>Under construction.</Text>
+            </View>
+        );
+    }
 }
 
 export default FeedScreen;
