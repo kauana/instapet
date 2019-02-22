642f2bb8fd66e9ef7033acf9a554496db0f1199f
Hanxing Zhang <35745247+681starflower@users.noreply.github.com>
2019-02-22 11:55:05 -0800

    add a new post to fire store; display posts from fire store; draft version

--- src/views/feed_screen.js
+++ src/views/feed_screen.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Dimensions,
+  Button,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import firebase from '../../firestore';
+
+const { width, height } = Dimensions.get('window');
+
+const Post = ({post}) => {
+  const comment =[{"name":"cm1"},{"name":"cm2"}];
+  const listItems = comment.map((d) => <Text key={d.name}>{d.name}</Text>);
+
+  return (
+    <View style={styles.imageContainer}>
+      <Image style={styles.image} resizeMode="cover" source={{ uri: post.image_url }} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{post.description} by {post.post_userID}</Text>
+        <Text style={styles.title}>{listItems}</Text>
+        <View style={styles.likesContainer}>
+          <Text style={styles.likes}>&hearts; {post.likes}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+
 
 class FeedScreen extends Component {
     static navigationOptions = () => ({
@@ -14,199 +45,105 @@ class FeedScreen extends Component {
         ),
     })
 
+
+
+    constructor() {
+      super();
+      this.ref = firebase.firestore().collection('posts');
+      this.unsubscribe = null;
+      this.state = {
+        posts: [],
+        loading: true,
+      };
+    }
+  
+    componentDidMount() {
+      this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate)
+    }
+  
+    componentWillUnmount() {
+      this.unsubscribe();
+    }
+  
+    onCollectionUpdate = (querySnapshot) => {
+      const posts = [];
+      querySnapshot.forEach((doc) => {
+        const { image_url, likes, description, post_userID, post_time_stamp, followers_ID, hashtag, commented_by_user } = doc.data();
+        posts.push({
+          key: doc.id, // Document ID
+          doc, // DocumentSnapshot
+          image_url,
+          likes,
+          description,
+          post_userID,
+          post_time_stamp,
+          followers_ID,
+          hashtag,
+          commented_by_user,
+        });
+      });
+      this.setState({
+        posts,
+        loading: false,
+     });
+    }
+  
+    addRandomPost = () => {
+      var author_ID_array = ['t3umcEmI187GfkibsYj7', 'MLVHiDjnWdVrw6QAr3fU', 'e1LkpFNjccmNnBMpIP6D'];
+      var random_author =  author_ID_array[Math.floor(Math.random() * author_ID_array.length)];
+      var hash_tag_array = ['cat','dog','rabbit'];
+      var random_hashtag = hash_tag_array[Math.floor(Math.random() * hash_tag_array.length)];
+
+      var random_comment = [];
+
+      for( var i=0; i< 2; i++) {
+        random_comment.push({
+                "t3umcEmI187GfkibsYj7":'cm' + Math.floor(Math.random()*10)
+        });
+      }
+
+      function add_Followers(post_userID) {
+        if (post_userID == 'MLVHiDjnWdVrw6QAr3fU') {
+          followers_ID = ['e1LkpFNjccmNnBMpIP6D', 't3umcEmI187GfkibsYj7']
+        } else if (post_userID == 'e1LkpFNjccmNnBMpIP6D') {
+          followers_ID = ['t3umcEmI187GfkibsYj7']
+        }
+        return followers_ID
+      }
+
+      this.ref.add({
+        description: "just added! a random post" + Math.floor((Math.random() * 100) + 1),
+        likes: Math.floor((Math.random() * 10) + 1),
+        image_url: 'https://source.unsplash.com/collection/190727/300x200',
+        post_userID: random_author,
+        post_time_stamp: firebase.firestore.FieldValue.serverTimestamp(),
+        followers_ID: add_Followers(random_author),
+        hashtag: [random_hashtag],
+        commented_by_user: random_comment,
+      });
+    }
+  
     render() {
+      if (this.state.loading) {
+        return <ActivityIndicator size="large" />;
+      }
+  
+      return (
+        <View style={styles.container}>
+          <FlatList
+            data={this.state.posts}
+            renderItem={({ item }) => <Post post={item}/>}
+          />
+          <Button title="Add random post" onPress={() => this.addRandomPost()} />
+        </View>
+      );
+    }
+
+/*     render() {
         // query against cloud store starts here
         const db = firebase.firestore();
 
-        // sortedObjected are posts sorted by time stamps
-        // show_TopK returns most recent K posts from each person app_user_id follows
-        function show_TopK(sortedObject,app_user_id,topK) {
-
-            var filteredObject = [];
-
-            // filteredObject are posts from people app_user_id follows
-            sortedObject.forEach((doc) => {
-                if (doc.data().followers_ID.indexOf(app_user_id) > -1) {
-                    filteredObject.push(doc);
-                }
-            });
-
-            console.log("filterd for app user");
-
-            var postFreqDict = {}
-            var topKfeeds = []
-            filteredObject.forEach((doc) => {
-                if (doc.data().post_userID in postFreqDict) {
-                    postFreqDict[doc.data().post_userID] += 1;
-                } else {
-                    postFreqDict[doc.data().post_userID] = 1;
-                }
-
-                if (postFreqDict[doc.data().post_userID] <= topK) {
-                    topKfeeds.push(doc)
-                }
-            });
-            console.log("after top K");
-
-            topKfeeds.forEach((doc) => {
-                console.log('most recent', topK,  'posts from each person t3umcEmI187GfkibsYj7 follows are ',
-                    doc.data().image_url, doc.data().post_userID, doc.data().post_time_stamp);
-            });
-
-        }
-
-        /*  // query 1: user profile details for user MLVHiDjnWdVrw6QAr3fU is
-         db.collection('users')
-           .doc('MLVHiDjnWdVrw6QAr3fU')
-           .get().then((doc) => {
-             if (doc.exists) {
-               console.log('user profile data for MLVHiDjnWdVrw6QAr3fU is :', doc.data());
-             } else {
-               // doc.data() will be undefined in this case
-               console.log('No such document!');
-             }
-           })
-           .catch((error) => {
-             console.log('Error getting document:', error);
-           });
-
-
-         // query 2: given a user, find all posts the user made
-         db.collection('posts')
-           .where('post_userID', '==', 'MLVHiDjnWdVrw6QAr3fU')
-           .get()
-           .then((querySnapshot) => {
-             querySnapshot.forEach((doc) => {
-               // doc.data() is never undefined for query doc snapshots
-               console.log('MLVHiDjnWdVrw6QAr3fU posted these pictures: ', doc.data().image_url);
-             });
-           })
-           .catch((error) => {
-             console.log('Error getting documents: ', error);
-           });
-
-
-         // query 3: given a user, find all users the user follows
-         db.collection('followed')
-           .doc('t3umcEmI187GfkibsYj7')
-           .get()
-           .then((doc) => {
-             if (doc.exists) {
-               console.log('app user t3umcEmI187GfkibsYj7 follows these users:', doc.data());
-             } else {
-               // doc.data() will be undefined in this case
-               console.log('No such document!');
-             }
-           })
-           .catch((error) => {
-             console.log('Error getting document:', error);
-           });
-
-
-         // query 4: given a user t3umcEmI187GfkibsYj7, find all posts from the people that the user follows
-         db.collection('posts')
-           .where('followers_ID', 'array-contains', 't3umcEmI187GfkibsYj7')
-           .get()
-           .then((querySnapshot) => {
-             querySnapshot.forEach((doc) => {
-               // doc.data() is never undefined for query doc snapshots
-               console.log('all posts should be shown for t3umcEmI187GfkibsYj7 are ', doc.data().image_url, doc.data().post_time_stamp);
-             });
-           })
-           .catch((error) => {
-             console.log('Error getting documents: ', error);
-           });
-
-
-         // query 5: given a hashtag, find all posts with this hashtag
-         db.collection('posts')
-           .where('hashtag', 'array-contains', 'snow')
-           .get()
-           .then((querySnapshot) => {
-             querySnapshot.forEach((doc) => {
-               // doc.data() is never undefined for query doc snapshots
-               console.log('posts contains hashtag "snow" are => ', doc.data().image_url, doc.data().hashtag);
-             });
-           })
-           .catch((error) => {
-             console.log('Error getting documents: ', error);
-           });
-
-
-         // query 6: given a post, find all comments for this post
-         db.collection('posts')
-           .doc('KOTXpXKf1BagqattgKt5')
-           .get()
-           .then((doc) => {
-             // doc.data() is never undefined for query doc snapshots
-             console.log('post KOTXpXKf1BagqattgKt5 has these comments: ', doc.data().commented_by_user);
-           })
-           .catch((error) => {
-             console.log('Error getting documents: ', error);
-           });
-
-         // query 7: given a post, find the followers that liked this post
-         db.collection('posts')
-           .doc('KOTXpXKf1BagqattgKt5')
-           .get()
-           .then((doc) => {
-             // doc.data() is never undefined for query doc snapshots
-             // console.log("these followers liked post KOTXpXKf1BagqattgKt5: ", doc.id, doc.data().liked_by_users);
-             console.log('these followers liked post KOTXpXKf1BagqattgKt5: ', doc.data().liked_by_users);
-           })
-           .catch((error) => {
-             console.log('Error getting documents: ', error);
-           });
-
-
-         // query 8: given a user, find all the posts that the user liked
-         db.collection('posts')
-           .where('liked_by_users', 'array-contains', 't3umcEmI187GfkibsYj7')
-           .get()
-           .then((querySnapshot) => {
-             querySnapshot.forEach((doc) => {
-               // doc.data() is never undefined for query doc snapshots
-               // console.log("t3umcEmI187GfkibsYj7 liked these posts => ",doc.id,  doc.data().image_url);
-               console.log('t3umcEmI187GfkibsYj7 liked these posts => ', doc.data().image_url);
-             });
-           })
-           .catch((error) => {
-             console.log('Error getting documents: ', error);
-           });
-      */
-
-
-
-        // test order by post_time_stamp
-        // query 9: show all posts, ordered by post_time_stamp
-        db.collection('posts')
-            .orderBy("post_time_stamp", 'desc')
-            .get()
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    // doc.data() is never undefined for query doc snapshots
-                    console.log('all posts order by post_time_stamp are ', doc.data().image_url, doc.data().post_time_stamp);
-                });
-            })
-            .catch((error) => {
-                console.log('Error getting documents: ', error);
-            });
-
-
-
-        // For app user t3umcEmI187GfkibsYj7, show most recent topK posts from
-        // each person t3umcEmI187GfkibsYj7 follows
-        const temp_order_by_time = db.collection('posts')
-            .orderBy('post_time_stamp', "desc");
-
-        var topK = 2;
-        temp_order_by_time.get()
-            .then((querySnapshot) => {
-                show_TopK(querySnapshot, "t3umcEmI187GfkibsYj7", topK)
-            })
-            .catch((error) => {
-                console.log('Error getting documents: ', error);
-            });
+ 
 
         return (
             <View style={{
@@ -219,7 +156,56 @@ class FeedScreen extends Component {
                 <Text>Under construction.</Text>
             </View>
         );
-    }
+    } */
 }
 
 export default FeedScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  imageContainer: {
+    width,
+    height: 315,
+    padding: 25,
+    backgroundColor: '#fefefe',
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    width: 300,
+    // height: 300,
+    marginBottom: 5,
+  },
+  textContainer: {
+    flexDirection: 'row',
+    alignContent: 'center',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 15,
+  },
+  title: {
+    flex: 4,
+  },
+  likesContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  headerContainer: {
+    width,
+    height: Platform.OS === 'ios' ? 70 : 50,
+    backgroundColor: '#fefefe',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+});
