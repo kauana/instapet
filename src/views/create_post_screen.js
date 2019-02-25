dfab4637ac1ab60868a68188f480344274a2777c
Kauana dos Santos <kau.grola@gmail.com>
2019-02-24 23:27:26 -0800

    Ask for camera permissions

--- src/views/create_post_screen.js
+++ src/views/create_post_screen.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import firebase from '../../firestore';
-import { Button, Input, Image } from 'react-native-elements';
-import colors from '../colors';
-import {ImagePicker} from 'expo';
+import { Button } from 'react-native-elements';
 import SettingsList from 'react-native-settings-list';
-
+import { ImagePicker, Permissions } from 'expo';
+import colors from '../colors';
+import firebase from '../../firestore';
 
 const styles = StyleSheet.create({
   icon: {
@@ -35,8 +34,8 @@ class CreatePostScreen extends Component {
       />
     ),
   })
- 
- constructor(props) {
+
+  constructor(props) {
     super(props);
     this.unsubscribe = null;
     const { navigation } = this.props;
@@ -46,164 +45,173 @@ class CreatePostScreen extends Component {
     this.state = {
       description: '',
       hashtag: '',
-      imagePath:{},
+      imagePath: '',
+      hasCameraPermission: false,
     };
   }
 
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
 
-handleUploadPhoto = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync();
+  handleUploadPhoto = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync();
 
     if (!result.cancelled) {
-      this.uploadImage(result.uri, "test-image")
+      this.uploadImage(result.uri, 'test-image')
         .then(() => {
-         console.log("success");
+          console.log('success');
         })
         .catch((error) => {
           console.log(error);
         });
     }
-}
+  }
 
- handleTakePhoto = async () => {
-    let result = await ImagePicker.launchCameraAsync();
-    if (!result.cancelled){
-        this.uploadImage(result.uri, "test-image")
+  handleTakePhoto = async () => {
+    const result = await ImagePicker.launchCameraAsync();
+    if (!result.cancelled) {
+      this.uploadImage(result.uri, 'test-image')
         .then(() => {
-         console.log("success");
+          console.log('success');
         })
         .catch((error) => {
           console.log(error);
         });
     }
-} 
+  }
 
 
-uploadImage = async (uri) => {
+  uploadImage = async (uri) => {
     const response = await fetch(uri);
     const blob = await response.blob();
 
-    let source = response;
+    const source = response;
     this.setState({
-        imagePath: source,
+      imagePath: source,
     });
 
-//    var ref = firebase.storage().ref.child("/images" + imageName);
-//    return ref.put(blob);
-}
+    //    var ref = firebase.storage().ref.child("/images" + imageName);
+    //    return ref.put(blob);
+  }
 
-postPhoto = () => {
+  postPhoto = () => {
     const {
-      description, hashtag, imagePath,
+      description, hashtag,
     } = this.state;
 
     this.write_ref.add({
-         description: this.state.description,
-         likes: false,
-       //  image_url: this.state.imagePath.data,
-         post_userID: this.userID,
-         post_time_stamp: firebase.firestore.FieldValue.serverTimestamp(),
-         hashtag: this.state.hashtag,
-       });
-}
+      description,
+      likes: false,
+      //  image_url: this.state.imagePath.data,
+      post_userID: this.userID,
+      post_time_stamp: firebase.firestore.FieldValue.serverTimestamp(),
+      hashtag,
+    });
+  }
 
 
   render() {
-    const db = firebase.firestore();
     const {
-          image_url, description, hashtag,
-        } = this.state;
-   
+      image_url, description, hashtag, hasCameraPermission,
+    } = this.state;
+
     return (
       <View style={{ backgroundColor: 'white', flex: 1 }}>
         <View style={{ flex: 1, marginTop: 0 }}>
-            <SettingsList borderColor={colors.grey(0.2)} defaultItemSize={50}>
+          <SettingsList borderColor={colors.grey(0.2)} defaultItemSize={50}>
             <SettingsList.Header headerText="CREATE POST" headerStyle={{ color: colors.grey(1), marginTop: 50 }} />
-          
-                <SettingsList.Item
-                    id="description"
-                    title="caption"
-                    isEditable
-                    hasNavArrow={false}
-                    value={description}
-                    onTextChange={text => this.setState({ description: text })}
-                    placeholder="About..."
-                    icon={(
-                    <Icon
-                      name="clipboard-text"
-                      size={24}
-                      color={colors.red(1)}
-                      style={styles.icon}
-                    />
-                  )}
-                />
-                <SettingsList.Item
-                    id="hashtag"
-                    title="hashtag"
-                    isEditable
-                    hasNavArrow={false}
-                    value={hashtag}
-                    onTextChange={text => this.setState({ hashtag: text })}
-                    placeholder="#"
-                    icon={(
-                    <Icon
-                      name="pound"
-                      size={24}
-                      color={colors.red(1)}
-                      style={styles.icon}
-                    />
-                  )}
-                />
-            </SettingsList>
-            <View style={styles.buttonContainer}>
-              <Button
-                title="Upload Photo"
-                activeOpacity={1}
-                underlayColor="transparent"
-                loadingProps={{ size: 'small', color: 'white' }}
-                buttonStyle={{
-                  height: 50,
-                  width: 250,
-                  backgroundColor: colors.red(1),
-                  borderWidth: 0,
-                }}
-                containerStyle={{ marginVertical: 10 }}
-                titleStyle={{ fontFamily: 'bold', color: 'white' }}
-                onPress={this.handleUploadPhoto}
-                
-              />
 
-             <Button
-                title="Take Photo"
-                activeOpacity={1}
-                underlayColor="transparent"
-                loadingProps={{ size: 'small', color: 'white' }}
-                buttonStyle={{
-                  height: 50,
-                  width: 250,
-                  backgroundColor: colors.red(1),
-                  borderWidth: 0,
-                }}
-                containerStyle={{ marginVertical: 10 }}
-                titleStyle={{ fontFamily: 'bold', color: 'white' }}
-                onPress={this.handleTakePhoto}
-              /> 
+            <SettingsList.Item
+              id="description"
+              title="caption"
+              isEditable
+              hasNavArrow={false}
+              value={description}
+              onTextChange={text => this.setState({ description: text })}
+              placeholder="About..."
+              icon={(
+                <Icon
+                  name="clipboard-text"
+                  size={24}
+                  color={colors.red(1)}
+                  style={styles.icon}
+                />
+              )}
+            />
+            <SettingsList.Item
+              id="hashtag"
+              title="hashtag"
+              isEditable
+              hasNavArrow={false}
+              value={hashtag}
+              onTextChange={text => this.setState({ hashtag: text })}
+              placeholder="#"
+              icon={(
+                <Icon
+                  name="pound"
+                  size={24}
+                  color={colors.red(1)}
+                  style={styles.icon}
+                />
+              )}
+            />
+          </SettingsList>
+          <View style={styles.buttonContainer}>
+            <Button
+              title="Upload Photo"
+              activeOpacity={1}
+              underlayColor="transparent"
+              loadingProps={{ size: 'small', color: 'white' }}
+              buttonStyle={{
+                height: 50,
+                width: 250,
+                backgroundColor: colors.red(1),
+                borderWidth: 0,
+              }}
+              containerStyle={{ marginVertical: 10 }}
+              titleStyle={{ fontFamily: 'bold', color: 'white' }}
+              onPress={this.handleUploadPhoto}
+            />
 
-             <Button
-                title="Post"
-                activeOpacity={1}
-                underlayColor="transparent"
-                loadingProps={{ size: 'small', color: 'white' }}
-                buttonStyle={{
-                  height: 50,
-                  width: 250,
-                  backgroundColor: colors.red(1),
-                  borderWidth: 0,
-                }}
-                containerStyle={{ marginVertical: 10 }}
-                titleStyle={{ fontFamily: 'bold', color: 'white' }}
-                onPress={this.postPhoto}
-              /> 
+            {
+            hasCameraPermission
+              ? (
+                <Button
+                  title="Take Photo"
+                  activeOpacity={1}
+                  underlayColor="transparent"
+                  loadingProps={{ size: 'small', color: 'white' }}
+                  buttonStyle={{
+                    height: 50,
+                    width: 250,
+                    backgroundColor: colors.red(1),
+                    borderWidth: 0,
+                  }}
+                  containerStyle={{ marginVertical: 10 }}
+                  titleStyle={{ fontFamily: 'bold', color: 'white' }}
+                  onPress={this.handleTakePhoto}
+                />
+              )
+              : null
+          }
+
+            <Button
+              title="Post"
+              activeOpacity={1}
+              underlayColor="transparent"
+              loadingProps={{ size: 'small', color: 'white' }}
+              buttonStyle={{
+                height: 50,
+                width: 250,
+                backgroundColor: colors.red(1),
+                borderWidth: 0,
+              }}
+              containerStyle={{ marginVertical: 10 }}
+              titleStyle={{ fontFamily: 'bold', color: 'white' }}
+              onPress={this.postPhoto}
+            />
 
           </View>
         </View>
